eed1162565407cbab6933aa4ec9f34ca5f710111
ouliyuan0129 <oleone@qq.com>
2025-04-25 11:57:22 +0800

    feat: 新增common模块并优化项目结构 1. 新增common模块用于存放公共代码 2. 依赖升级: 升级Spring Boot到2.7.10, Java到17, transformer到1.2.10, 优化MyBatis相关依赖 3. 删除冗余代码和构建脚本 4. 修复OrderBuyResponse包名引用

--- template/src/main/resources/archetype-resources/__artifactId__-api/src/main/java/__packageInPathFormat__/order/api/module/response/OrderBuyResponse.java
+++ template/src/main/resources/archetype-resources/__artifactId__-api/src/main/java/__packageInPathFormat__/order/api/module/response/OrderBuyResponse.java
@@ -1,11 +1,11 @@
 package ${package}.order.api.module.response;
 
-import com.sun.tms.order.api.module.dto.OrderBuyDTO;
+import ${package}.order.api.module.dto.OrderBuyDTO;
 import com.transformer.response.Result;
 import lombok.Data;
 
 @Data
-public class OrderBuyResponse<T> extends Result<OrderBuyDTO> {
+public class OrderBuyResponse extends Result<OrderBuyDTO> {
     private static final OrderBuyResponse DEFAULT_RESPONSE = new OrderBuyResponse();
 
     public static OrderBuyResponse empty() {
