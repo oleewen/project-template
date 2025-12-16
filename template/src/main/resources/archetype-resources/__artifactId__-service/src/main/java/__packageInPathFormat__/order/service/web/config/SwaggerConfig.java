0d7a722ce8a1d4ff9bea13f0bb725895bab67792
ouliyuan0129 <oleone@qq.com>
2025-12-16 17:48:31 +0800

    refactor: 重构infrastructure模块包结构，将点号分隔包名改为目录结构
    - 删除旧的点号分隔包结构文件（common.resource, goods.infrastructure, order.infrastructure）
    - 新增目录结构包（common/, goods/, order/）
    - 更新SwaggerConfig配置

--- template/src/main/resources/archetype-resources/__artifactId__-service/src/main/java/__packageInPathFormat__/order/service/web/config/SwaggerConfig.java
+++ template/src/main/resources/archetype-resources/__artifactId__-service/src/main/java/__packageInPathFormat__/order/service/web/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package ${package}.service.web.config;
+package ${package}.order.service.web.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
